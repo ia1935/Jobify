74109fbeffef5e56480a2117ab1e551139a226c0
Imran Ahmed <imranhahmed0987@gmail.com>
2025-01-05 22:58:02 -0500

    autofilling

--- src/appFill.py
+++ src/appFill.py
@@ -4,3 +4,38 @@
 #also need to have logging for websites that program had errors with for manual entry
 #need to also connect this to a excel spreadsheet.
 
+
+import time
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+
+
+
+def jobApplication(links:list):
+
+    
+    resume_info = '../data/resume_info.json'
+
+    with open(resume_info,'r') as file:
+        resume_data = json.load(file)
+
+    driver = webdriver.Chrome()
+
+    linkIssues=[]
+
+    for link in links:
+        try:
+            driver.get(link)
+
+        except:
+            print("Error processing Link")
+            linkIssues.append(link)
+            continue
+    
+    return linkIssues
+
+
--- src/main.py
+++ src/main.py
@@ -9,25 +9,33 @@ from dataRetrieval import GetUserInfo, resumeScraper
 
 
 def main():
-    #grabbing filename
-    file_name = GetUserInfo()
-    if not file_name:
-        print("No File entered. Ending program")
-        return
 
-
-    extracted_data = resumeScraper(file_name)
-        
-    if extracted_data:
-        print("Extracted Resume Data:")
-        print(extracted_data)
-    else:
-        print("No data extracted.")
-    # print("Getting links from GitHub Page:\n")
-    # apply_links = WebScraper()
     
-    # print("Printing Links:")
-    # printLinks()
+
+
+
+#implementation of user input resume to parse and enter info in job link. For scalability in future for others to use this can be implemented
+
+    # #grabbing filename
+    # file_name = GetUserInfo()
+    # if not file_name:
+    #     print("No File entered. Ending program")
+    #     return
+
+
+    # extracted_data = resumeScraper(file_name)
+        
+    # if extracted_data:
+    #     print("Extracted Resume Data:")
+    #     # print(extracted_data)
+    # else:
+    #     print("No data extracted.")
+
+    applyLinks = WebScraper()
+
+    #need to now pass this array to my application function to apply for jobs
+    
+    
 
 
 
